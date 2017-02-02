4fa85a7408d68505bdeeec4815161ac6d22bf289
funzuhdi <alfanzuhdi28@gmail.com>
2017-02-02 07:47:39 +0700

    adding files

---
+++ TicketBooking/Controllers/BookingsController.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TicketBooking.Models;
+using System.IO;
+
+namespace TicketBooking.Controllers
+{
+    public class BookingsController : Controller
+    {
+        TicketBookingEntities db = new TicketBookingEntities();
+
+        // GET: Bookings
+        public ActionResult Index()
+        {
+            return View(db.Bookings.ToList());
+        }
+
+        // GET: Bookings/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        [HttpGet]
+        public ActionResult Create()
+        {
+            ViewBag.IdStatus = new SelectList(db.Status, "IdStatus", "StatusPerjalanan");
+            return View();
+        }
+
+        // POST: Bookings/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Date,From,To,Passenger,Status")] Booking booking, Status status)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Bookings.Add(booking);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(booking);
+        } 
+
+        // GET: Bookings/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // POST: Bookings/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "BookingId,Date,From,To,Passenger,Status")] Booking booking)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(booking).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(booking);
+        }
+
+        // GET: Bookings/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Booking booking = db.Bookings.Find(id);
+            if (booking == null)
+            {
+                return HttpNotFound();
+            }
+            return View(booking);
+        }
+
+        // POST: Bookings/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Booking booking = db.Bookings.Find(id);
+            db.Bookings.Remove(booking);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+        
+    }
+}
---
+++ TicketBooking/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TicketBooking.Startup))]
+namespace TicketBooking
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
